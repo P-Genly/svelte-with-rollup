66f752de2bed55a22737c00e1537832dd13f4ff5
Paul Genly <paul.genly.partner@decathlon.com>
2024-04-09 14:58:23 +0200

    Add babel to use your built components with older ECMAScript versions in target project

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
-import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import babel from "@rollup/plugin-babel";
 
 function createConfig(file) {
     return {
@@ -11,9 +12,8 @@ function createConfig(file) {
             { file: `dist/${file}.js`, format: 'es' }
         ],
         plugins: [
-            svelte({
-                preprocess: vitePreprocess()
-            }),
+            babel({ babelHelpers: 'bundled' }),
+            svelte({ preprocess: vitePreprocess() }),
             resolve(),
             commonjs(),
             terser(),
--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
     export let title: string;
     export let onClick = () => {
         alert('Hi everybody!')
     }
+
+    onMount(() => {
+        console.log('test');
+    })
 </script>
 
 <div>
